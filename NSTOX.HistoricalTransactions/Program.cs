3ef1c74203d0fa675db260aad1aa909a17ae11ff
MatthewSteeples <matthew@mercuryit.co.uk>
2013-02-27 11:41:50 +0000

    Use a single instance mutex

--- NSTOX.HistoricalTransactions/Program.cs
+++ NSTOX.HistoricalTransactions/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace NSTOX.HistoricalTransactions
 {
     static class Program
     {
+        static Mutex instanceMutex = null;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -14,9 +18,26 @@ namespace NSTOX.HistoricalTransactions
         static void Main()
         {
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Application.ApplicationExit += Application_ApplicationExit;
+            bool createdNew;
+            instanceMutex = new Mutex(true, @"Local\" + Assembly.GetExecutingAssembly().GetType().GUID, out createdNew);
+            if (!createdNew)
+            {
+                instanceMutex = null;
+                return;
+            } 
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MenuForm());
+        }
+
+        static void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            if (instanceMutex != null)
+            {
+                instanceMutex.Close();
+            }
         }
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
