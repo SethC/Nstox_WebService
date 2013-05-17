5b57d1ae1137d6dcd752ab0d080aaa9d50549b68
Matthew Steeples <matthew@mercuryit.co.uk>
2013-05-17 21:43:34 +0100

    Implement MVC

---
+++ NSTOX.WebService/Controllers/DiagnosticsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NSTOX.WebService.Controllers
+{
+    public class DiagnosticsController : Controller
+    {
+        public ActionResult Logs(string date = null)
+        {
+            if (string.IsNullOrEmpty(date))
+            {
+                date = DateTime.Today.ToString("yyyy-MM-dd");
+            }
+            return View();
+        }
+
+    }
+}
--- NSTOX.WebService/Global.asax.cs
+++ NSTOX.WebService/Global.asax.cs
@@ -7,6 +7,8 @@ using System.Web.SessionState;
 using Microsoft.WindowsAzure;
 using System.Configuration;
 using System.Diagnostics;
+using System.Web.Routing;
+using System.Web.Mvc;
 
 namespace NSTOX.WebService
 {
@@ -23,6 +25,8 @@ namespace NSTOX.WebService
             });
             CloudTraceListener.StorageTraceListener.Instance.Write("Initialised");
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            RegisterRoutes(RouteTable.Routes);
         }
 
         void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
@@ -64,6 +68,20 @@ namespace NSTOX.WebService
         protected void Application_End(object sender, EventArgs e)
         {
 
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*allsvc}", new { allsvc = @".*\.svc(/.*)?" });
+
+            routes.MapRoute("Home", "", new { controller = "Home", action = "Ria" });
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional, option = UrlParameter.Optional } // Parameter defaults
+            );
         }
     }
 }
